a69c8a509b7f6492b7ad7d1ca0d89ea30e544d6e
zmzhang <8111354+zmzhang8@users.noreply.github.com>
2024-09-26 17:01:35 -0700

    Implement the dictionary UI for the popup

--- src/scripts/popup.ts
+++ src/scripts/popup.ts
@@ -1,31 +1,61 @@
 document.addEventListener("DOMContentLoaded", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
     if (tab.id) {
-      document
-        .getElementById("openSidePanelBtn")
-        ?.addEventListener("click", () => {
-          openSidePanel(tab.id!);
-          window.close();
-        });
+      setupAIButton(tab.id);
 
       chrome.tabs.sendMessage(
         tab.id,
         { action: "getSelectedContent" },
         (selectedContent: string) => {
-          document.getElementById("popupText")!.textContent = selectedContent;
+          if (selectedContent) {
+            (
+              document.getElementById("lookup-input") as HTMLInputElement
+            ).value = selectedContent;
+          } else {
+            (
+              document.getElementById("lookup-input") as HTMLInputElement
+            ).focus();
+          }
         }
       );
     } else {
-      console.error("No active tab found");
+      (document.getElementById("lookup-input") as HTMLInputElement).focus();
     }
   });
+
+  setupLookupButton();
 });
 
-async function openSidePanel(tabId: number) {
-  await chrome.sidePanel.setOptions({
-    tabId: tabId,
-    path: "sidepanel.html",
-    enabled: true,
+function setupAIButton(tabId: number) {
+  const aiButton = document.getElementById("ai-button") as HTMLButtonElement;
+  aiButton.classList.add("active");
+  aiButton.addEventListener("click", () => {
+    chrome.sidePanel
+      .setOptions({
+        tabId: tabId,
+        path: "sidepanel.html",
+        enabled: true,
+      })
+      .then(() => {
+        chrome.sidePanel.open({ tabId: tabId });
+      });
+    window.close();
   });
-  await chrome.sidePanel.open({ tabId: tabId });
+}
+
+function setupLookupButton() {
+  const lookupInput = document.getElementById(
+    "lookup-input"
+  ) as HTMLInputElement;
+  const lookupButton = document.getElementById(
+    "lookup-button"
+  ) as HTMLButtonElement;
+
+  lookupInput.addEventListener("input", () => {
+    if (lookupInput.value.trim() !== "") {
+      lookupButton.classList.add("active");
+    } else {
+      lookupButton.classList.remove("active");
+    }
+  });
 }
